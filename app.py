f704f6963bd164281c9acdc2db10a1bcb6333e3d
danilousGois <133375156+danilousGois@users.noreply.github.com>
2024-03-12 14:25:06 +0000

    atualizações

--- app.py
+++ app.py
@@ -2,23 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-    return render_template('menu_inicial.html')
-
-
-@app.route('/filipi')
-def filipi():
-    return render_template('filipi.html')
-
-@app.route('/ronald')
-def ronald():
-    return render_template('ronald.html')
+@app.route('/perfil/<user>/<cor>')
+def perfil(user, cor):
+    return render_template('perfil.html', usuario=user, cor=cor)
 
 #render_template vai sempre procurar na pasta 'templates'
 #a pasta templates tem que estar na mesma hierarquia do venv e do __pycache__
 
 #criar o gitignore na linha de comando: touch .gitignore
+
+
 
 
 if __name__ == '__main__':
