c18afe4b478371c9937a68660c9fc36233f55daa
danilousGois <133375156+danilousGois@users.noreply.github.com>
2024-08-02 18:45:48 +0000

    salvando momentaneamente

--- app.py
+++ app.py
@@ -1,18 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/perfil/<user>/<cor>')
-def perfil(user, cor):
-    return render_template('perfil.html', usuario=user, cor=cor)
 
-#render_template vai sempre procurar na pasta 'templates'
-#a pasta templates tem que estar na mesma hierarquia do venv e do __pycache__
-
-#criar o gitignore na linha de comando: touch .gitignore
+@app.route('/')
+def index():
+   return render_template('paginainicial.html')
 
 
+@app.route('/<valor>')
+def login_signin():
+   renderizar_arquivo = ''
+
+   if valor == 'login':
+      renderizar_arquivo = 'login'
+   else:
+      renderizar_arquivo = 'signin'
+
+   return render_template(renderizar_arquivo + '.html')
+
+@app.route('/signin', methods=['POST'])
+def validarsignin():
+   email = request.form['email']
+   senha = request.form['senha']
+   confirmarsenha = request.form['confirmarsenha']
+
+   if senha != confirmarsenha:
+      
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login')
+def verificarlogin():
+   if email != '' and senha != '':
+
+
+
+
+@app.route('/<materia>')
+def carregarmateria(materia):
+
+   materias = {
+      'Matemática': {
+         'nome': 'matemática',
+         'conteudos': ['análise combinatória', 'funções', 'trigonometria', 'geometria', 'algebra']
+      }, 
+      'Física': {
+         'nome': 'física',
+         'conteudos': ['dinâmica', 'eletrodinâmica', 'eletromagneteismo', 'óptica', 'calorimetria']
+      },
+      'Química': {
+         'nome': 'química',
+         'conteudos': ['reações químicas', 'química inorgânica', 'química orgânica', 'estequiometria', 'balanceamento', 'cinética química']
+      },
+      'Português': {
+         'nome': 'português',
+         'conteudos': ['análise sintática', 'interpretação de texto', 'sequências textuais', 'coesão e coerência']
+      },
+      'Biologia': {
+         'nome': 'biologia',
+         'conteudos': ['citologia', 'reações metabólicas', 'histologia', 'anatomia e fisiologia', 'microbiologia']
+      },
+      'História': {
+         'nome': 'história',
+         'conteudos': ['idade média', 'idade antiga', 'américa espanhola', 'renascimento','Rgito antigo', 'Grécia antiga']
+      }
+   }
+
+
+   return render_template('carregarmaterias.html', materia=materias[materia])
+
+
+
+if __name__ == "__main__":
+    app.run
