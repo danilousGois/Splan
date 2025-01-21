0c1c428db78329749562ac44c62eda3545536a07
danilousGois <danilopereirag1@gmail.com>
2025-01-21 09:37:31 -0300

    salvando script temporario de login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, make_response, session
 import json
 import os
+import uuid
 from flask_migrate import Migrate
 from utils import db
 from controllers.Usuario import user_bp
@@ -39,3 +40,143 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+def carregar_materias():
+   if os.path.exists('static/materias.json'):
+      with open('static/materias.json', 'r') as json_file:
+         materias = json.load(json_file)
+      return materias
+   return {}
+
+def verificar_user_logado():
+   ID_Usuario = request.cookies.get('ID_Usuario')
+   if ID_Usuario == None:
+      return False
+   if os.path.exists('static/dados_usuario.json'):
+      with open('static/dados_usuario.json', 'r') as json_file:
+         lista_usuarios = json.load(json_file)
+         for lista_dict in lista_usuarios:
+               if lista_dict.get('ID_Usuario') == ID_Usuario:
+                  return True
+   return False
+
+@app.route('/')
+def index():
+   if verificar_user_logado() == True:
+      return redirect('/inicio')
+   return render_template('paginainicial.html')
+
+@app.route('/inicio')
+def carregarLandingPage():
+   if 'ID_Usuario' not in session:
+      return redirect('/autenticar')
+   nomeuser = session['nomeuser']
+   return render_template('base_landingpage.html', nomeuser = nomeuser)
+
+
+
+@app.route('/<valor>')
+def paginainicial(valor):
+   if verificar_user_logado() == True:
+      return redirect('/inicio')
+   elif valor == 'autenticar':
+      return render_template('login.html')
+   else:
+      return render_template('signup.html')
+
+@app.route('/formulario')
+def formulario():
+   return render_template('formulario.html')
+
+
+@app.route('/cadastrar', methods=['POST'])
+def validarsignup():
+   nomeuser = request.form['nome']
+   emailUser = request.form['email']
+   senhaUser = request.form['senha']
+   confirmarsenha = request.form['confirmarsenha']
+   ID_Usuario = str(uuid.uuid4())
+
+   if emailUser and senhaUser and confirmarsenha:
+      if senhaUser == confirmarsenha:
+         if os.path.exists('static/dados_usuario.json'):
+            with open('static/dados_usuario.json', 'r') as json_file:
+               lista_usuarios = json.load(json_file)
+            for lista_dict in lista_usuarios:
+               if emailUser == lista_dict['email']:
+                  flash('E-mail já cadastrado, faça login!', "primary")
+                  return redirect('/autenticar')
+         else:
+            lista_usuarios = []
+
+         dados_user = {
+            "ID_Usuario": ID_Usuario,
+            "nomeuser": nomeuser,
+            "email": emailUser,
+            "senha": senhaUser
+         }
+
+         lista_usuarios.append(dados_user)
+         with open('static/dados_usuario.json', 'w') as json_file:
+            json.dump(lista_usuarios, json_file, indent=4)
+
+         session['ID_Usuario'] = ID_Usuario
+         session['nomeuser'] = nomeuser
+         session['email'] = emailUser     
+
+         return redirect('/inicio')
+      
+      else:
+         flash('Senha e confirmação devem ser iguais!', "warning")
+         return redirect('/cadastrar')
+   else:
+      flash('Todos os campos devem ser preenchidos!', "warning")
+      return redirect('/cadastrar')
+
+
+@app.route('/autenticar', methods=['POST'])
+def verificarlogin():
+   email = request.form['email']
+   senha = request.form['senha']
+   Ver_email = False
+   Ver_senha = False
+   with open('static/dados_usuario.json', 'r') as json_file:
+      lista_usuarios = json.load(json_file)
+
+   if email and senha:
+      for lista_dict in lista_usuarios:
+         if email == lista_dict['email'] and senha == lista_dict['senha']:
+            Ver_email = True
+            Ver_senha = True
+            session['ID_Usuario'] = lista_dict['ID_Usuario']
+            session['nomeuser'] = lista_dict['nomeuser']
+            session['email'] = lista_dict['email']
+            return redirect('/inicio')
+
+      if not (Ver_email and Ver_senha):
+         flash('Esse usuário não existe! Faça o cadastro!')
+         return redirect('/cadastrar')
+   else:
+      flash('Todos os campos devem ser preenchidos!')
+      return redirect('/autenticar')
+
+
+@app.route('/inicio/<materia>')
+def carregarmateria(materia):
+   nomeuser = session['nomeuser']
+   materias = carregar_materias()
+   if materia in materias:
+      return render_template('carregarmaterias.html', materia=materias[materia], nomeuser = nomeuser)
+   else:
+      return "Matéria não encontrada", 404
+
+
+@app.route('/logout')
+def logout():
+   resp = make_response(redirect('/autenticar'))
+   resp.delete_cookie('ID_Usuario')
+   return resp
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
--- scripts.py
+++ scripts.py
@@ -3,14 +3,3 @@
 #somar os pesos das materias
 #dividir o tempo total pela soma dos pesos
 #distribuir essa unidade de tempo para cada materia(multiplicar essa unidade de tempo pelo peso da materia para obter o tempo total)
-#
-
-from flask import Flask
-from controllers.Usuario import user_bp
-from utils import db
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.secret_key = 'secret_key_splan'
-
-app.register_blueprint(user_bp, url_prefix='/usuario')
