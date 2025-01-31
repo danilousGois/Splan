fb4ff2e2b3630286d540965c47ad4a1480e2cbae
danilousGois <danilopereirag1@gmail.com>
2025-01-31 14:46:26 -0300

    crud de usuario funcionando

--- controllers/Usuario.py
+++ controllers/Usuario.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, make_response, session, Blueprint
 from models.Usuario import Usuario
-from utils import db
+from utils import db, login_manager
 import hashlib
+from flask_login import current_user
+from flask_login import login_user, logout_user, login_required
 
 user_bp = Blueprint('usuario', __name__, template_folder='templates')
 
@@ -33,10 +35,15 @@ def criar_usuario():
     db.session.add(user)
     db.session.commit()
 
+    session['user'] = user.nome
+
     return render_template('base_landingpage.html', nome=user.nome)
 
 
-
+@login_manager.user_loader
+def load_user():
+    usuario = Usuario.query.filter_by(id=session.id_usuario).first()
+    return usuario
 
 
 #usar sessão de usuario para guardar o user logado e fazer o crud
@@ -44,6 +51,9 @@ def criar_usuario():
 @user_bp.route('/login', methods=['POST', 'GET'])
 def login_usuario():
     if request.method == 'GET':
+        if 'user' in session:
+            return render_template('base_landingpage.html', nome=session['user'])
+        
         return render_template('login.html')
     
     email = request.form['email']
@@ -54,12 +64,50 @@ def login_usuario():
     if user:
         hash_senha = hashlib.sha256(senha.encode()).hexdigest()
         if user.senha == hash_senha:
-            return render_template('base_landingpage.html', nome=user.nome)
+            session['user'] = user.nome
+            return render_template('base_landingpage.html', nome=session['user'])
         else:
             flash('Senha incorreta!', 'warning')
             return render_template('login.html')
     else:
         flash('There is no user bounded with this email', 'warning')
         return render_template('login.html')
+    
 
 
+
+@user_bp.route('/alterarperfil', methods=['POST'])
+def update_user():
+    user = Usuario.query.filter_by(nome=session['user']).first()
+    user.nome = request.form['nome']
+    user.email = request.form['email']
+    user.telefone = request.form['telefone']
+    user.senha = request.form['senhaNew']
+
+    session['user'] = user.nome
+
+    db.session.add(user)
+    db.session.commit()
+    
+    return render_template('base_landingpage.html', nome=session['user'])
+
+
+@user_bp.route('/deletarperfil')
+def deletar_user():
+    user = Usuario.query.filter_by(nome=session['user']).first()
+    db.session.delete(user)
+    db.session.commit()
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+
+@user_bp.route('/logoff')
+def logoff():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+
+@user_bp.route('/perfil')
+def carregar_perfil():
+    user = Usuario.query.filter_by(nome=session['user']).first()
+    return render_template('perfil_user.html', user=user)
